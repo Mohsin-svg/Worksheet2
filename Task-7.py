56db20507e2ffa9b9fa32a4e2d7d31133989dc87
Mohsin-svg <12029494@stanleycollege.edu.au>
2024-10-19 04:24:59 +0800

    Create Task-7.py

---
+++ Task-7.py
@@ -0,0 +1,6 @@
+courses = ["Introduction to Philosophy", "Data Structures and Algorithms", "Calculus I", "Biology I", "Physics I", 
+           "Microeconomics", "Calculus II", "Linear Algebra", "History I", "Chemistry I", "Psychology I", 
+           "Macroeconomics", "English Composition I", "Introduction to Programming", "Discrete Mathematics"]
+courses.sort(reverse=True)  
+
+print("Reverse alphabetical order:", courses)
