8997f65934608f7ec1b74c8f37f4941f28cef41b
Mohsin-svg <12029494@stanleycollege.edu.au>
2024-10-19 04:21:17 +0800

    Create Task-2.py

---
+++ Task-2.py
@@ -0,0 +1,3 @@
+courses = ["Introduction to Philosophy", "Data Structures and Algorithms", "Calculus I", "Biology I", "Physics I", "Microeconomics", "Calculus II", "Linear Algebra", "History I", "Chemistry I", "Psychology I", "Macroeconomics", "English Composition I", "Introduction to Programming", "Discrete Mathematics"]
+sorted_courses = sorted(courses)
+print(sorted_courses)
