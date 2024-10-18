c1e98441fefab4806af45193b1ea137b8edb6ffc
Mohsin-svg <12029494@stanleycollege.edu.au>
2024-10-19 04:23:24 +0800

    Create Task-5.py

---
+++ Task-5.py
@@ -0,0 +1,3 @@
+['Discrete Mathematics', 'Introduction to Programming', 'English Composition I', 'Macroeconomics', 'Psychology I', 'Chemistry I', 'History I', 'Linear Algebra', 'Calculus II', 'Microeconomics', 'Physics I', 'Biology I', 'Calculus I', 'Data Structures and Algorithms', 'Introduction to Philosophy']
+courses.reverse()
+print(courses)
